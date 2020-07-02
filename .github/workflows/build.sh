329a632ab3d8a7e279e5a60d6bfe385d4312d432
Paul Thomson <paulthomson@google.com>
2020-07-02 22:16:02 +0100

    Remove -v

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -100,7 +100,7 @@ esac
 mkdir -p build
 pushd build
   cmake -G Ninja .. -DCMAKE_BUILD_TYPE="${CONFIG}" "${CMAKE_OPTIONS[@]}"
-  cmake --build . --config "${CONFIG}" -- -v
+  cmake --build . --config "${CONFIG}"
   cmake -DCMAKE_INSTALL_PREFIX=./install -DBUILD_TYPE="${CONFIG}" -P cmake_install.cmake
 popd
 
